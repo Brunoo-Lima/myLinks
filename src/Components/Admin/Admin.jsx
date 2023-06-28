dad2855322c913c17495d8433c645e38449ec253
Bruno Lima <bruno2.jean.lima@outlook.com>
2023-06-28 20:06:56 -0300

    firebase integration

--- src/Components/Admin/Admin.jsx
+++ src/Components/Admin/Admin.jsx
@@ -1,33 +1,104 @@
+import { useState } from 'react';
 import './Admin.css';
 import { Header } from '../../Components/Header/Header';
 import { Logo } from '../../Assets/Logo/Logo';
 import Input from '../Form/Input';
 import { MdAddLink } from 'react-icons/md';
 import { FiTrash2 } from 'react-icons/fi';
+import { toast } from 'react-toastify';
+
+import { database } from '../../services/FirebaseConection';
+import {
+  addDoc,
+  collection,
+  onSnapshot,
+  query,
+  orderBy,
+  doc,
+  deleteDoc,
+} from 'firebase/firestore';
 
 const Admin = () => {
+  const [nameInput, setNameInput] = useState('');
+  const [urlInput, setUrlInput] = useState('');
+  const [backgroundColorInput, setBackgroundColorInput] = useState('#f1f1f1');
+  const [textColorInput, setTextColorInput] = useState('#121212');
+
+  async function handleRegister(event) {
+    event.preventDefault();
+    if (nameInput === '' || urlInput === '') {
+      toast.warn('Preencha todos os campos');
+      return;
+    }
+
+    //addDoc gera id aleatorio
+    addDoc(collection(database, 'links'), {
+      name: nameInput,
+      url: urlInput,
+      bg: backgroundColorInput,
+      color: textColorInput,
+      created: new Date(),
+    })
+      .then(() => {
+        setNameInput('');
+        setUrlInput('');
+      })
+      .catch(() => {
+        toast.error('Erro ao salvar o link');
+      });
+  }
+
   return (
     <div className="adminContainer">
       <Header />
       <Logo />
-      <form className="form">
+      <form className="form" onSubmit={handleRegister}>
         <label className="label">Nome do Link</label>
-        <Input placeholder="Nome do link..." />
+        <Input
+          value={nameInput}
+          onChange={({ target }) => setNameInput(target.value)}
+          placeholder="Nome do link..."
+        />
 
         <label className="label">Url do Link</label>
-        <Input type="url" placeholder="Digite aqui..." />
+        <Input
+          type="url"
+          value={urlInput}
+          onChange={({ target }) => setUrlInput(target.value)}
+          placeholder="Digite aqui..."
+        />
 
         <section className="containerColor">
           <div>
             <label className="label right">Fundo do link</label>
-            <input type="color" />
+            <input
+              type="color"
+              value={backgroundColorInput}
+              onChange={({ target }) => setBackgroundColorInput(target.value)}
+            />
           </div>
 
           <div>
             <label className="label right">Cor do link</label>
-            <input type="color" />
+            <input
+              type="color"
+              value={textColorInput}
+              onChange={({ target }) => setTextColorInput(target.value)}
+            />
           </div>
         </section>
+
+        {nameInput !== '' && (
+          <div className="preview">
+            <label className="label">Pre-visualização</label>
+            <article
+              className="linkList"
+              style={{ marginTop: 8, backgroundColor: backgroundColorInput }}
+            >
+              <p style={{ color: textColorInput }}>{nameInput}</p>
+            </article>
+          </div>
+        )}
 
         <button className="btn">
           Cadastrar <MdAddLink size={24} color="#fff" />
